f60769f073f9dc477b1957c164a128fddb9aa6eb
Dhluis <exclusicoparaclientes@gmail.com>
2025-10-25 18:38:20 -0600

    disque ya va afuncionar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,13 @@
+"use client"
 
+import { DashboardMetrics } from "@/components/dashboard-metrics"
+import { TestInventoryFunctions } from "@/components/test-inventory-functions"
+
+export default function Home() {
+  return (
+    <div className="flex-1 space-y-4 p-8 pt-6">
+      <DashboardMetrics />
+      <TestInventoryFunctions />
+    </div>
+  )
+}
